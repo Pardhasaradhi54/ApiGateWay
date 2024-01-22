783c01e2fafb672e7c044c32972fac80139a6fab
Pardhasaradhi54 <156801108+Pardhasaradhi54@users.noreply.github.com>
2024-01-22 12:59:34 +0530

    Add files via upload

---
+++ src/main/java/com/wissen/agentrealai/services/apigateway/util/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.wissen.agentrealai.services.apigateway.util;
+
+import java.security.Key;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+	public static final String SECRET = "examportalL1232222222222222222222222222222222222222222222223333333333333333333";
+
+	public void validateToken(final String token) {
+		Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+	}
+
+	private Key getSignKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+}
+
+
